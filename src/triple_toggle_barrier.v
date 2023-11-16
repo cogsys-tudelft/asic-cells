b7ba6103189f027c03207e0824f57bc729e6068c
Douwe den Blanken <douwedb@gmail.com>
2023-11-16 12:36:13 +0100

    Make at posedge reset optional for triple toggle barrier

--- src/triple_toggle_barrier.v
+++ src/triple_toggle_barrier.v
@@ -4,6 +4,7 @@
 `include "double_latching_barrier.v"
 
 module triple_toggle_barrier
+    #(parameter AT_POSEDGE_RST = 1)
     (
         input clk,
         input rst,
@@ -16,20 +17,36 @@ module triple_toggle_barrier
 
     wire in_sync;
 
-    double_latching_barrier double_latching_barrier_inst (
+    double_latching_barrier #(
+        .AT_POSEDGE_RST(AT_POSEDGE_RST)
+    ) double_latching_barrier_inst (
         .clk(clk), .rst(rst), .enable(enable),
         .in(in), .out(in_sync)
     );
 
     reg in_delete;
 
-    always @(posedge clk, posedge rst) begin
-        if (rst) begin
-            in_delete <= 0;
-        end else if (enable) begin
-            in_delete <= in_sync;
+    // Unfortunately, the approach below is the only way to create synthesizable
+    // Verilog code. It is not possible to only generate the always @ (...) part.
+    generate
+        if (AT_POSEDGE_RST == 1) begin
+            always @(posedge clk, posedge rst) begin
+                if (rst) begin
+                    in_delete <= 0;
+                end else if (enable) begin
+                    in_delete <= in_sync;
+                end
+            end
+        end else begin
+            always @(posedge clk) begin
+                if (rst) begin
+                    in_delete <= 0;
+                end else if (enable) begin
+                    in_delete <= in_sync;
+                end
+            end
         end
-    end
+    endgenerate
 
     assign out = in_sync & ~in_delete;
 
