525d5da4e4cc40c3d26d04ddd625438b7cc5b15d
Douwe den Blanken <douwedb@gmail.com>
2024-01-25 18:06:50 +0100

    Create wide domain crossing registers

---
+++ src/domain_crossing/wide_double_delay_register.v
@@ -0,0 +1,30 @@
+`ifndef __WIDE_DOUBLE_DELAY_REGISTER_V__
+`define __WIDE_DOUBLE_DELAY_REGISTER_V__
+
+`include "double_delay_register.v"
+
+module wide_double_delay_register #(
+    parameter WIDTH = 8
+) (
+    input clk,
+    input rst,
+
+    input  [WIDTH-1:0] in,
+    output [WIDTH-1:0] in_delayed
+);
+
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin
+            double_delay_register double_delay_register_inst (
+                .clk(clk),
+                .rst(rst),
+
+                .in(in[i]),
+                .in_delayed(in_delayed[i])
+            );
+        end
+    endgenerate
+endmodule
+
+`endif
---
+++ src/domain_crossing/wide_double_latching_barrier.v
@@ -0,0 +1,33 @@
+`include "double_latching_barrier.v"
+
+module wide_double_latching_barrier #(
+    parameter WIDTH = 8,
+    parameter AT_POSEDGE_RST = 1
+) (
+    input clk,
+    input rst,
+
+    input enable,
+
+    input  [WIDTH-1:0] in,
+    output [WIDTH-1:0] out
+);
+
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin
+            double_latching_barrier #(
+                .AT_POSEDGE_RST(AT_POSEDGE_RST)
+            ) double_latching_barrier_inst (
+                .clk(clk),
+                .rst(rst),
+
+                .enable(enable),
+
+                .in (in[i]),
+                .out(out[i])
+            );
+        end
+    endgenerate
+
+endmodule
