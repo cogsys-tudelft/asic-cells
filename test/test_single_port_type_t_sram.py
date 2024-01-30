ee302c1b1ef9a6a211220aaecace3ec086799df8
Douwe den Blanken <douwedb@gmail.com>
2024-01-30 11:32:59 +0100

    Rename single-port SRAM to type t SRAM

--- test/test_single_port_type_t_sram.py
+++ test/test_single_port_type_t_sram.py
@@ -6,8 +6,8 @@ from cocotb_test.simulator import run
 
 
 @pytest.mark.parametrize("parameters", [{"WIDTH": "8", "NUM_ROWS": "128"}, {"WIDTH": "3", "NUM_ROWS": "47"}, {"WIDTH": "128", "NUM_ROWS": "256"}])
-def test_single_port_tsmc_sram(parameters):
-    module_name = "single_port_tsmc_sram"
+def test_single_port_type_t_sram(parameters):
+    module_name = "single_port_type_t_sram"
 
     file_dir = Path(__file__).resolve().parent
     source_dir = str(file_dir / ".." / "src" / "sram")
@@ -18,10 +18,10 @@ def test_single_port_tsmc_sram(parameters):
         toplevel=module_name,
         module=f"tests.{module_name}_tests",
         parameters=parameters,
-        compile_args=[f"+incdir+{source_dir}"], #  '--x-assign unique', '--x-initial unique'
+        # compile_args=[f"+incdir+{source_dir}"], #  '--x-assign unique', '--x-initial unique'
         extra_args=["--trace", "--coverage"], # Store a VCD file in the sim_build directory
     )
 
 
 if __name__ == "__main__":
-    test_single_port_tsmc_sram({"WIDTH": "8", "NUM_ROWS": "128"})
+    test_single_port_type_t_sram({"WIDTH": "8", "NUM_ROWS": "128"})
