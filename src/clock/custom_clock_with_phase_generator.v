66fa2068849bfd8d8a32adda55e1cdc1e162c2a2
Douwe den Blanken <douwedb@gmail.com>
2024-03-05 13:47:33 +0100

    Add note on clock reset for clock generator

--- src/clock/custom_clock_with_phase_generator.v
+++ src/clock/custom_clock_with_phase_generator.v
@@ -16,13 +16,13 @@ module custom_clock_with_phase_generator
 
     always @(posedge clk_in, posedge arst) begin
         if (arst) begin
-            counter <= 0;
+            counter <= 1; // Reset counter to 1, so that if one cycle is specified for a phase, 'counter == cycles' will be true in the first clock cycle
             max_count <= low_phase_cycles;
             clk_out <= 1'b0;
         // Make sure that the clock generator is not running unnecessarily
         end else if (enable) begin
             if (counter == cycles) begin
-                counter <= 0;
+                counter <= 1; // Reset to 1 for the same reason as above
                 max_count <= (max_count == low_phase_cycles) ? high_phase_cycles : low_phase_cycles;
                 clk_out <= ~clk_out;
             end else begin
